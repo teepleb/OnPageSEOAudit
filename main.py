4d456bd887a23d31ada167156af866ef1937cdaa
Blake Teeple <teepleblake@gmail.com>
2018-05-16 16:20:26 -0400

    Started Building Header Tag Logic
    Started implementing header tag logic with counting and saving off values.

--- main.py
+++ main.py
@@ -26,14 +26,39 @@ class Parser():
         self.title = ""
         self.desc = ""
         self.canonical = ""
+        self.header_count = {"h1": 0, "h2": 0, "h3": 0, "h4": 0, "h5": 0, "h6": 0}
         self.headers = {}
         self.parseHTML(page_source)
     
     def parseHTML(self, html_string):
         parsed_html = BeautifulSoup(html_string, "lxml")
+
         self.title = parsed_html.head.find('title').text
+
         self.desc = parsed_html.head.find('meta', attrs={'name': 'description'})['content']
+
         self.canonical = parsed_html.head.find('link', attrs={'rel': 'canonical'})['href']
+    
+        self.header_count['h1'] = len(parsed_html.body.find_all('h1'))
+        self.header_count['h2'] = len(parsed_html.body.find_all('h2'))
+        self.header_count['h3'] = len(parsed_html.body.find_all('h3'))
+        self.header_count['h4'] = len(parsed_html.body.find_all('h4'))
+        self.header_count['h5'] = len(parsed_html.body.find_all('h5'))
+        self.header_count['h6'] = len(parsed_html.body.find_all('h6'))
+
+        if self.header_count['h1'] > 1:
+            self.headers['h1'] = []
+            for value in parsed_html.body.find_all('h1'):
+                self.headers['h1'].append(value.text)
+        else:
+            self.headers['h1'] = parsed_html.body.find('h1')
+
+        if self.header_count['h2'] > 1:
+            self.headers['h2'] = []
+            for value in parsed_html.body.find_all('h2'):
+                self.headers['h2'].append(value.text)
+        else:
+            self.headers['h2'] = parsed_html.body.find('h2')
 
     @property
     def getTitle(self):
@@ -49,7 +74,7 @@ class Parser():
 
     @property
     def getHeaders(self):
-        return self.headers
+        return self.headers, self.header_count
 
 def printIntro(section_title):
     # This function will print the header for each section in a nicer way, for now.
@@ -81,8 +106,8 @@ if __name__ == '__main__':
         printIntro("DESCRIPTION")
         # description - content, length, etc.
         print("Your DESC is: {0}".format(parser.getDesc))
-        if len(parser.getDesc) >= 300:
-            print("WARNING: Your DESC is too long. You should reduce it to be less than 300 characters for best display.")
+        if len(parser.getDesc) > 160:
+            print("WARNING: Your DESC is too long. You should reduce it to be less than 160 characters for best display.")
         elif len(parser.getDesc) <= 50:
             print("WARNING: You're missing out on SERP space! Add some more content to your description. Keep in mind it needs to be between 50-300 characters for best performance.")
 
@@ -99,8 +124,20 @@ if __name__ == '__main__':
 
         printIntro("HEADERS")
         # header tags (h1 x 1, h2, h3, etc.) - need to show frequency, h1 highest on page, and header tag content
+        print("The current amount of headers are: \n")
         if parser.getHeaders:
-            print("HEADERS")
+            headers, header_count = parser.getHeaders
+            print("H1 -> {0}".format(header_count['h1']))
+            print("H2 -> {0}".format(header_count['h2']))
+            print("H3 -> {0}".format(header_count['h3']))
+            print("H4 -> {0}".format(header_count['h4']))
+            print("H5 -> {0}".format(header_count['h5']))
+            print("H6 -> {0}".format(header_count['h6']))
+
+            if header_count['h1'] > 1:
+                print("WARNING: You have more than one H1 on your page. Please reduce to having only one on the page.")
+            elif header_count['h1'] == 0:
+                print("WARNING: You do not have an H1 on your page, please add one.")
 
         printIntro("INTERNAL LINKING")
         # on page linking - internal vs external, no follow vs follow, broken links, no special chars in urls aka they are friendly
